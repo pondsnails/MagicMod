3b2147369cb945a8a238e9ba58a81581513d74cb
BuildTools <unconfigured@null.spigotmc.org>
2021-08-05 19:59:42 +0900

    Forge 1.16.5 MagicMod

--- src/main/java/jp/kps8x9/m2/magicmod/items/MagicWandBase.java
+++ src/main/java/jp/kps8x9/m2/magicmod/items/MagicWandBase.java
@@ -17,15 +17,10 @@ import net.minecraft.particles.ParticleTypes;
 import net.minecraft.util.*;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.vector.Vector3d;
-import net.minecraft.util.text.IFormattableTextComponent;
-import net.minecraft.util.text.ITextComponent;
-import net.minecraft.util.text.StringTextComponent;
-import net.minecraft.util.text.Style;
 import net.minecraft.world.Explosion;
 import net.minecraft.world.World;
 
 import java.awt.*;
-import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
 
